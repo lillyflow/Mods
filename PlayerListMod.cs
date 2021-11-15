654c4049938d7dfe5d06629a9b6c009a26113e2d
MalooMF9 <86484052+Adnezz@users.noreply.github.com>
2021-11-14 19:59:40 -0600

    Updated version number and author info

--- PlayerListMod.cs
+++ PlayerListMod.cs
@@ -8,7 +8,7 @@ using UnhollowerRuntimeLib;
 using UnityEngine;
 using VRChatUtilityKit.Components;
 
-[assembly: MelonInfo(typeof(PlayerList.PlayerListMod), "PlayerList_JEFFMOD", "1.6.3", "Adnezz", "https://github.com/loukylor/VRC-Mods")]
+[assembly: MelonInfo(typeof(PlayerList.PlayerListMod), "PlayerList", "2.0.0", "Adnezz", "https://github.com/Adnezz/PlayerList")]
 [assembly: MelonGame("VRChat", "VRChat")]
 [assembly: MelonOptionalDependencies("UIExpansionKit", "emmVRC")]
 
